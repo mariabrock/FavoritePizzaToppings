b6a13f3809e0aa80b0474df7ed9c0c4fd589b052
Maria <mariabrock414@gmail.com>
2020-03-23 21:20:45 -0500

    Add project files.

---
+++ FavoritePizzaToppings/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FavoritePizzaToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"./pizzas.json"));
+
+            var toppingLists = pizzas.Select(p => string.Join(", ", p.Toppings.OrderBy(t => t)));
+
+            //var distinctToppingCombinations = toppingLists.Distinct();
+
+            var countOfCombinations = new Dictionary<string, int>();
+
+            foreach (var combination in toppingLists)
+            {
+                if (!countOfCombinations.ContainsKey(combination))
+                {
+                    countOfCombinations.Add(combination, 1);
+                }
+                else
+                {
+                    countOfCombinations[combination] += 1;
+                }
+            }
+
+            var mostOrdered = countOfCombinations.OrderByDescending(item => item.Value).Take(20);
+
+            foreach (var (combination, count) in mostOrdered)
+            {
+                Console.WriteLine($"The topping combination of {combination} was ordered {count} times.");
+            }
+        }
+    }
+}
